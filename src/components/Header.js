92ba79e0eb485c3fa46320b1e5ea401dd691d21c
Hemalatha Mylraj <52548798+hemarajan0808@users.noreply.github.com>
2022-09-26 15:21:06 -0400

    Redux counter app creation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,34 @@
-import classes from './Header.module.css';
+import { useSelector, useDispatch } from "react-redux";
+import { authActions } from "../store/auth";
+import classes from "./Header.module.css";
 
 const Header = () => {
-  return (
-    <header className={classes.header}>
-      <h1>Redux Auth</h1>
+   const dispatch = useDispatch();
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
+  const logoutHandler = () => {
+    dispatch(authActions.logout());
+  };
+
+  <header className={classes.header}>
+
+   <h1>Redux Auth</h1>
+   
+   { isAuth && (
       <nav>
         <ul>
           <li>
-            <a href='/'>My Products</a>
+            <a href="/">My Products</a>
           </li>
           <li>
-            <a href='/'>My Sales</a>
+            <a href="/">My Sales</a>
           </li>
           <li>
-            <button>Logout</button>
+            <button onClick={logoutHandler}>Logout</button>
           </li>
         </ul>
       </nav>
-    </header>
-  );
+    )}
+  </header>; 
 };
-
 export default Header;
